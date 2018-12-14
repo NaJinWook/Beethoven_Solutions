43217f95b1a40b01291aa4e857c8beae2543f566
석훈 이 <nolru7@naver.com>
2018-12-14 18:07:26 +0900

    rdb모듈 + rdb이벤트 + rdb 추가

---
+++ Beethoven/WindowsFormsApp1/ob_Rdb.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class ob_Rdb
+    {
+        Form form;
+        string name, text;
+        int sX, sY, pX, pY;
+
+        public ob_Rdb(Form form, string name, string text, int sX, int sY, int pX, int pY)
+        {
+            this.form = form;
+            this.name = name;
+            this.text = text;
+            this.sX = sX;
+            this.sY = sY;
+            this.pX = pX;
+            this.pY = pY;
+
+        }
+        public Form Form
+        {
+            get
+            {
+                return form;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+        }
+        public int SX
+        {
+            get
+            {
+                return sX;
+            }
+        }
+        public int SY
+        {
+            get
+            {
+                return sY;
+            }
+        }
+        public int PX
+        {
+            get
+            {
+                return pX;
+            }
+        }
+        public int PY
+        {
+            get
+            {
+                return pY;
+            }
+        }
+    }
+}
